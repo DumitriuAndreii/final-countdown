0d006e7536aea4808ca0d0078199abe65cbc9b72
DumitriuAndreii <andrei24ian@gmail.com>
2024-03-20 19:25:45 +0200

    Update result modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useImperativeHandle } from "react";
 
-export const ResultModal = ({result, targetTime,reference}) => {
+export const ResultModal = ({ result, targetTime }, ref) => {
+  useImperativeHandle(reference);
+
   return (
-    <dialog ref={reference} className='result-modal'>
-        <h2>{result}</h2>
-        <p>the target time was {targetTime} seconds.</p>
-        <p>you stopped the timer with X seconds left</p>
-        <form method='dialog'>
-            <button>Close</button>
-        </form>
+    <dialog ref={ref} className="result-modal">
+      <h2>{result}</h2>
+      <p>the target time was {targetTime} seconds.</p>
+      <p>you stopped the timer with X seconds left</p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
     </dialog>
-  )
-}
+  );
+};
